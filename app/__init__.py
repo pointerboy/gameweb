68e42ff91a5c52d2d49fd370eafc811da1d36749
Nerijus Pocevičius <nerijuspocevicius45@gmail.com>
2019-06-10 18:44:05 +0300

    Added validations, now notifies you in reg sys

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, flash, url_for, session, redirect
 from .config import AUTH_SQLITE_URI, SECRET_KEY
 from flask_sqlalchemy import SQLAlchemy
+import re
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = AUTH_SQLITE_URI
@@ -44,11 +46,19 @@ def register_route():
         #TODO: Handle emails and usernames that are already being used
         if not request.form['email'] or not request.form['pwd'] or not request.form['confirmpwd']:
             flash('Please enter all the fields', 'error')
-            return redirect(url_for('landing'))
+            return render_template('usercontrol/register.html')
+
+        if not re.match("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", request.form['email']):
+            flash('Invalid email', 'error')
+            return render_template('usercontrol/register.html')
+
+        if db.session.query(User.id).filter_by(email=request.form['email']).scalar() is not None:
+            flash("E-Mail already exists", 'error')
+            return render_template('usercontrol/register.html')
 
         if request.form['pwd'] != request.form['confirmpwd']:
             flash('Passwords do not match', 'error')
-            return redirect(url_for('landing'))
+            return render_template('usercontrol/register.html')
 
         session['email'] = request.form['email']
         session['pwd'] = request.form['pwd']
@@ -60,20 +70,23 @@ def register_route():
 @app.route('/register_username', methods=['GET', 'POST'])
 def register_username():
     if not 'email' in session or not 'pwd' in session:
-            flash('Whacha doin?')
-            return redirect(url_for('/'))
+        return redirect(url_for('landing'))
 
     if request.method == "POST":
         #TODO: Duplicate username
         if not request.form['username']:
             flash("You need to fill out the username.", "error")
-            return redirect(url_for('/'))
+            return render_template('usercontrol/register_username.html')
+
+        if db.session.query(User.id).filter_by(username=request.form['username']).scalar() is not None:
+            flash("Username already exists", 'error')
+            return render_template('usercontrol/register_username.html')
 
         user = User(request.form['username'], session['email'], session['pwd'])
         
         db.session.add(user)
         db.session.commit()
 
-        flash("You got registered scrub")
+        flash("You have been successfully registered!")
         return redirect(url_for('landing'))
     return render_template("usercontrol/register_username.html")
